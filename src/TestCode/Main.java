e2da228cfda115f3c6e9bc472142cfb04e87de3f
Dmitry Vochaev <dvochaev@mail.ru>
2024-05-13 01:36:27 +0300

    Revert "1"
    This reverts commit 9aa89771c2892759bc7e7fbdb8e415068c3878a2.

--- src/TestCode/Main.java
+++ src/TestCode/Main.java
@@ -1,29 +1,10 @@
 package TestCode;
 
 
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-       String list = String.valueOf("Good news everyone!".length());
-
-
-
-
-
-
-
-        System.out.println(list instanceof String);
-
-
-        ;
-
-           }
-           }
-
-
-
-
-
+    }
+}
 
