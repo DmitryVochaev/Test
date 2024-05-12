9aa89771c2892759bc7e7fbdb8e415068c3878a2
Dmitry Vochaev <dvochaev@mail.ru>
2024-05-13 01:23:51 +0300

    1

--- src/TestCode/Main.java
+++ src/TestCode/Main.java
@@ -1,10 +1,29 @@
 package TestCode;
 
 
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-    }
-}
+       String list = String.valueOf("Good news everyone!".length());
+
+
+
+
+
+
+
+        System.out.println(list instanceof String);
+
+
+        ;
+
+           }
+           }
+
+
+
+
+
 
