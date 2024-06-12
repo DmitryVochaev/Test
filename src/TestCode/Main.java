216285de3b011b7325456a869a983d73dbf395ae
Dmitry Vochaev <dvochaev@mail.ru>
2024-06-12 13:36:47 +0300

    "Students marks"

--- src/TestCode/Main.java
+++ src/TestCode/Main.java
@@ -1,14 +1,37 @@
 package TestCode;
 
 
-import java.util.Scanner;
-import java.util.Set;
 
-public class Main {
+import java.util.Random;
+
+
+public class Main{
     public static void main(String[] args) {
+        System.out.println("Student marks");
+
+        Random random = new Random();
+        int count = random.nextInt(4)+2;
+        System.out.println(count);
 
 
+
+
+        switch (count) {
+            case 2 :
+                System.out.println("bad mark");
+                break;
+            case 3 :
+                System.out.println("satisfactory mark");
+                break;
+            case 4 :
+                System.out.println("good mark");
+                break;
+            case 5 :
+                System.out.println("excellent mark");
+                break;
+            default:
+                System.out.println("Error mark");
+        }
     }
-    }
-
+}
 
